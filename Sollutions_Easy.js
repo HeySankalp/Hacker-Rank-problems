567dd2af1dca1aec7f167d96ec13aa3e9d223ccf
HeySankalp <sankalpsachan1@gmail.com>
2023-02-05 14:12:49 +0530

    divisible sum pair sollution

--- Sollutions_Easy.js
+++ Sollutions_Easy.js
@@ -82,3 +82,18 @@ function birthday(s, d, m) {
     }
     return noOfSegments;
 }
+
+
+//Q:-  Given an array of integers and a positive integer k , determine the number of (i,j)  pairs where  i<j and  ar[i] + ar[j]  is divisible by k .
+
+function divisibleSumPairs(n, k, ar) {
+    let pairCount = 0
+    for(let i= 0; i<n-1; i++){
+        for(let j=i+1; j<n; j++){
+            if((ar[i]+ar[j])%k===0){
+                pairCount+=1;
+            }
+        }
+    }
+    return pairCount;
+}
