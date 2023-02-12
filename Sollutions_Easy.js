20b11cecb0be3f47ca5bc74cf3f53d8313d448f1
HeySankalp <sankalpsachan1@gmail.com>
2023-02-12 19:12:33 +0530

    cat and mouse problem

--- Sollutions_Easy.js
+++ Sollutions_Easy.js
@@ -235,3 +235,18 @@ function getMoneySpent(keyboards, drives, b) {
     }
     return maxPurchase;
 }
+
+
+// Q :- Two cats and a mouse are at various positions on a line. You will be given their starting positions. Your task is to determine
+//  which cat will reach the mouse first, assuming the mouse does not move and the cats travel at equal speed. If the cats arrive
+//   at the same time, the mouse will be allowed to move and it will escape while they fight.
+
+function catAndMouse(x, y, z) {
+    if(Math.abs(z-x)<Math.abs(z-y)){
+            return 'Cat A'
+        }else if(Math.abs(z-x)>Math.abs(z-y)){
+            return 'Cat B'
+        }else{
+            return 'Mouse C'
+        }
+    }
